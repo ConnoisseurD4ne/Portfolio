57220ce6502b421adfdc0b9c23d52ccb82156fc4
ConnoisseurD4ne <164378406+ConnoisseurD4ne@users.noreply.github.com>
2024-03-22 00:11:02 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+/* ===== typed js. Modified text and animation speed ==== */
+const typed = new Typed('.multiple-text', {
+    strings: ['Animator', 'Illustrator', 'Graphic Designer', 'Gamer'],
+    typeSpeed: 65,
+    backSpeed: 65,
+    backDelay: 1000,
+    loop: true,
+});
